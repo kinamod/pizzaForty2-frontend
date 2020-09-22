74986074452b208d2273be4a745b31e36b2bbfa6
Kinamod <kina2k@gmail.com>
2020-09-22 10:39:37 +0100

    removed isLogging concole log

--- src/utils/logger-helper.js
+++ src/utils/logger-helper.js
@@ -1,6 +1,6 @@
 const isLogging = process.env.REACT_APP_LOGGING_ON || false;
 const logger = (sender , message) => {
-    console.log("isLogging: " + isLogging);
+    //console.log("isLogging: " + isLogging);
     if(isLogging) { console.log(sender+": - "+ message); }
   }
   
