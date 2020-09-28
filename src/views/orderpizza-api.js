8355301690f7c727c31326dd6508993ba2ee9311
Kinamod <kina2k@gmail.com>
2020-09-28 16:00:26 +0100

    clean ui and fixe verify email button visibility

--- src/views/orderpizza-api.js
+++ src/views/orderpizza-api.js
@@ -6,7 +6,6 @@ import { logger } from "../utils/logger-helper";
 
 export const OrderPizzaApi = () => {
   const [message, setMessage] = useState("");
-  // const { handleShowModal, handleCloseModal } = BetterModal();
   const apiUrl = (process.env.REACT_APP_RUNNING_LOCALLY) ? process.env.REACT_APP_API_URL_LOCAL : process.env.REACT_APP_API_URL;
   const { user, getAccessTokenSilently } = useAuth0();
 
@@ -14,9 +13,11 @@ export const OrderPizzaApi = () => {
   const [showModal, setShowModal] = useState(false);
   const [modalTitle, setModalTitle] = useState("Default Title");
   const [modalMessage, setModalMessage] = useState("Default Message");
+  const [modalButton, setModalButton] = useState("OK");
+  const [modalEmailVerified, setEmailVerified] = useState(false);
   const handleCloseModal = () => setShowModal(false);
   const handleShowModal = () => setShowModal(true);
-  //the modal
+  //END the modal
 
   const callApi = async () => {
 
@@ -24,7 +25,6 @@ export const OrderPizzaApi = () => {
       logger("callApi", "backendAPI: " + apiUrl);
 
       const response = await fetch(`${apiUrl}/api/public-message`);
-
       const responseData = await response.json();
 
       setMessage(responseData);
@@ -34,10 +34,9 @@ export const OrderPizzaApi = () => {
   };
 
 
-
-
   const orderPizza = async () => {
     var responseData = "";
+    setEmailVerified(user.email_verified)
     if (user.email_verified) {
       try {
         const token = await getAccessTokenSilently();
@@ -51,8 +50,14 @@ export const OrderPizzaApi = () => {
         });
 
         responseData = await response.json();
+        console.log(responseData)
         setModalTitle("Pizza Ordered");
-        setModalMessage(`Thanks ${user.name}, your pizza has been ordered.\nHave a nice day!`);
+        if (responseData.pizzaStatus) {
+          setModalMessage(`Thanks for your order ${user.name}, ${responseData.msg}!`);
+        } else {
+          setModalMessage(`Sorry ${user.name}, there was a problem!`);
+        }
+        setModalButton("Great Thanks!")
         handleShowModal();
 
       } catch (error) {
@@ -60,6 +65,8 @@ export const OrderPizzaApi = () => {
       }
     } else {
       responseData = "You must first verify your email address.";
+
+      setModalButton("Resend Verification Email")
       setModalTitle("Unverified Email Address");
       setModalMessage("You must first verify your email address.");
       handleShowModal();
@@ -81,6 +88,13 @@ export const OrderPizzaApi = () => {
         <Button onClick={orderPizza} color="primary" className="mt-5">
           Order My Pizza
         </Button>
+        {/* Testing API call buttons
+        <Button onClick={sendVerifyEmailLink} variant="secondary" className="mt-5">
+          Verify Email Test
+        </Button>
+        <Button onClick={getFullID} variant="secondary" className="mt-5">
+          Get Full ID
+        </Button> */}
       </ButtonGroup>
 
 
@@ -98,13 +112,67 @@ export const OrderPizzaApi = () => {
         </Modal.Header>
         <Modal.Body>{modalMessage}</Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={handleCloseModal}>
-            OK
-          </Button>
+          {modalEmailVerified ?
+            <Button variant="primary" onClick={handleCloseModal}>
+              {modalButton}
+            </Button>
+            :
+            <Button variant="primary" onClick={sendVerifyEmailLink}>
+              {modalButton}
+            </Button>
+          }
         </Modal.Footer>
       </Modal>
     </Container>
   );
+
+  //Functions
+  async function sendVerifyEmailLink() {
+
+    try {
+      logger("verifyemail", "backendAPI: " + apiUrl);
+
+      const token = await getAccessTokenSilently();
+      logger("verifyemail", "email verified: " + user.email_verified);
+      logger("verifyemail", "token: " + token)
+
+      const response = await fetch(`${apiUrl}/api/verify-email`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          UserID: user.sub
+        },
+      });
+
+      const responseData = await response.json();
+      setMessage(responseData);
+    } catch (error) {
+      setMessage(error.message);
+    }
+  }
+
+  // Testing functions
+  // async function getFullID() {
+
+  //   try {
+  //     logger("fullID", "backendAPI: " + apiUrl);
+
+  //     const token = await getAccessTokenSilently();
+  //     logger("fullID", "token: " + token)
+
+  //     const response = await fetch(`${apiUrl}/api/get-full-id`, {
+  //       headers: {
+  //         Authorization: `Bearer ${token}`,
+  //         UserID: user.sub,
+  //       },
+  //     });
+
+  //     const responseData = await response.json();
+  //     setMessage(responseData);
+  //   } catch (error) {
+  //     setMessage(error.message);
+  //   }
+  // }
 };
 
 export default OrderPizzaApi;
+
